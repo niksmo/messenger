9bf4d5bbb13e7876ddebb9d2c09911421e61e501
Nikolay <n.smolov@yandex.ru>
2023-11-17 19:32:02 +0300

    adds settings nav list and refactored partials
    * adds nav-item
    
    * adds settings nav list and refactored partials

---
+++ src/partials/components/dropdown/menu-item-type.ts
@@ -0,0 +1,7 @@
+import type { TIcon24 } from '../../../types/icon';
+
+export interface IMenuItem {
+  style: 'primary' | 'adverse';
+  icon: TIcon24;
+  label: string;
+}
--- src/scripts/edit-profile.ts
+++ src/scripts/edit-profile.ts
@@ -1,5 +1,4 @@
 import '../styles/index.css';
 import '../styles/pages/edit-profile.css';
-import '../partials/layouts/form/form.css';
 import '../partials/components/input/input.css';
 import '../partials/components/button/button.css';
