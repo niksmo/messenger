a14f7d780747e742f9dee24b24b8452e2627ca25
nikolay <n.smolov@yandex.ru>
2023-11-17 19:31:06 +0300

    adds settings nav list and refactored partials

--- src/scripts/change-password.ts
+++ src/scripts/change-password.ts
@@ -1,5 +1,4 @@
 import '../styles/index.css';
 import '../styles/pages/change-password.css';
-import '../partials/layouts/form/form.css';
 import '../partials/components/input/input.css';
 import '../partials/components/button/button.css';
--- src/scripts/signup.ts
+++ src/scripts/signup.ts
@@ -1,5 +1,4 @@
 import '../styles/index.css';
 import '../styles/pages/signup.css';
-import '../partials/layouts/form/form.css';
 import '../partials/components/input/input.css';
 import '../partials/components/button/button.css';
