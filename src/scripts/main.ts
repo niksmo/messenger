08e703dbd9509b0f561e4bd46d66bea230b82ff4
nikolay <n.smolov@yandex.ru>
2023-11-18 19:08:22 +0300

    adds chat top-bar

--- src/scripts/main.ts
+++ src/scripts/main.ts
@@ -2,5 +2,7 @@ import '../styles/index.css';
 import '../styles/pages/main.css';
 import '../partials/components/sidebar/sidebar.css';
 import '../partials/components/chat-plug/chat-plug.css';
+import '../partials/components/chat/chat.css';
 
 import '../partials/components/sidebar/sidebar';
+import '../partials/components/chat/chat';
