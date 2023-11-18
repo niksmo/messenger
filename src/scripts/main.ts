badcff5b64f730da1142c0da5b97f4c09d92dcfa
nikolay <n.smolov@yandex.ru>
2023-11-18 17:29:11 +0300

    adds chat plug and refactor blocks

--- src/scripts/main.ts
+++ src/scripts/main.ts
@@ -1,5 +1,6 @@
 import '../styles/index.css';
 import '../styles/pages/main.css';
 import '../partials/components/sidebar/sidebar.css';
+import '../partials/components/chat-plug/chat-plug.css';
 
 import '../partials/components/sidebar/sidebar';
